17ffa68a9a4ad0ae0085c2682f9b222d15f0ab2b
chirodev <65997245+chirodev@users.noreply.github.com>
2021-11-27 22:53:43 -0400

    Add files via upload

---
+++ 6.Esquema Basico VI/Controladores/AccionAnimacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormularioGrafica.Controladores
+{
+    class AccionAnimacion
+    {
+        public string accion { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float AnguloX { get; set; }
+        public float AnguloY { get; set; }
+        public float AnguloZ { get; set; }
+        public float tiempo { get; set; }
+        public float cantidad { get; set; }
+
+    }
+}
---
+++ 6.Esquema Basico VI/Objetos/IObjeto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Mathematics;
+namespace Juego
+{
+    public abstract class IObjeto : Interface
+    {
+        public Dictionary<string, IObjeto> listadepartes= new Dictionary<string, IObjeto>();
+        public static bool rotacionTotal;
+        public abstract void Dibujar();
+        public abstract void rotar(float angle, float x, float y, float z,Vector3 origen);
+        public abstract void Traslacion(float trasX, float trasY, float trasZ);
+        public abstract void Escala(float num);
+
+    }
+}
